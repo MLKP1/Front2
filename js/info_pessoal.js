4d5a50665591ecfe5d8863ca13b33fb9450bb89d
Kxxaa1 <kaua.kauafelix2007@gmail.com>
2025-08-13 12:04:55 -0300

    incompleto

---
+++ js/info_pessoal.js
@@ -0,0 +1,7 @@
+const alterar = document.getElementById('alterar-senha')
+const excluir = document.getElementById('excluir-conta')
+
+excluir.addEventListener('click', async function () {
+   
+}) 
+    
